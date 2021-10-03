50911d3f20aa5470b2b1d67936d27b9f05779373
Diego Aguilera <diegoaguilera4@icloud.com>
2021-10-03 20:27:37 -0300

    Agregamos directorio path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-
+import path from 'path';
 //const express = require('express');
 //const morgan = require('morgan');
 //const cors=require('cors');
@@ -18,6 +18,8 @@ app.set('port', process.env.PORT || 4000);
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.urlencoded({extende:true}));
+app.use(express.static(path.join(__dirname,'public')));
 app.use(cors());
 
 //Routes
@@ -29,4 +31,5 @@ app.use(express.static(__dirname + '/public'));
 //Server is listening
 app.listen(app.get('port'), () => {
     console.log('Server on port', app.get('port'));
+    console.log(path.join(__dirname,'public'));
 });
